1147f8737ef55a9fe04204fb9302cacc99912448
Leonid <zemlyanykh@bk.ru>
2025-08-18 12:33:54 +0500

    Added fetch helper

---
+++ frontend/src/helper/fetchHelper.tsx
@@ -0,0 +1,34 @@
+type TypeFetchOptions = {
+  url: string;
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  body?: object;
+};
+
+export const fetchHelper = ({ method, url, body }: TypeFetchOptions) => {
+  const basicURL = "https://students.netoservices.ru/fe-diplom";
+
+  const options: RequestInit = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...(body && { body: JSON.stringify(body) }),
+  };
+
+  return fetch(basicURL + url, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.status}`);
+      }
+
+      if (response.status === 204) {
+        return null;
+      }
+
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(`Ошибка от сервера: ${error}`);
+      throw new Error(`Ошибка от сервера: ${error}`);
+    });
+};
