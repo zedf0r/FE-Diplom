f75c8a69fb1fa05a32e1d30ef6e34d14454569ee
Leonid <zemlyanykh@bk.ru>
2025-10-01 13:20:14 +0500

    Update repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/FE-Diplom/",
-});
+  base: command === "build" ? "/FE-Diplom/" : "/",
+}));
